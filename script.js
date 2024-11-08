ba84a83bccbfd02ea1933eaf2c8628714deee3c1
vincent munywoki <vincentmunywoki12@gmail.com>
2024-11-09 01:11:04 +0300

    initial commit

--- script.js
+++ script.js
@@ -74,3 +74,22 @@ switchMode.addEventListener('change', function () {
 		document.body.classList.remove('dark');
 	}
 })
+
+window.addEventListener('resize', function () {
+	if(this.innerWidth > 576) {
+		searchButtonIcon.classList.replace('bx-x', 'bx-search');
+		searchForm.classList.remove('show');
+	}
+})
+
+
+
+const switchMode = document.getElementById('switch-mode');
+
+switchMode.addEventListener('change', function () {
+	if(this.checked) {
+		document.body.classList.add('dark');
+	} else {
+		document.body.classList.remove('dark');
+	}
+})
